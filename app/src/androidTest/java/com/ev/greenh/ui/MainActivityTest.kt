0981afa87300bde32adb54553bd5838a7b2525ff
Sudhanshu Kumar <rishuparashar7@gmail.com>
2023-07-23 23:44:33 +0530

    mockito setup

---
+++ app/src/androidTest/java/com/ev/greenh/ui/MainActivityTest.kt
@@ -0,0 +1,17 @@
+package com.ev.greenh.ui
+
+import org.junit.Rule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
+/*
+ * Created by Sudhanshu Kumar on 22/07/23.
+ */
+
+class MainActivityTest {
+
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+
+
+}
