bc72f51384873dce758c3af9fe6f03189f978163
Sudhanshu Kumar <rishuparashar7@gmail.com>
2024-02-26 22:57:50 +0530

    turbine

--- auth/src/test/java/com/example/auth/domain/usecase/SendOtpUseCaseTest.kt
+++ auth/src/test/java/com/example/auth/domain/usecase/SendOtpUseCaseTest.kt
@@ -1,10 +1,12 @@
 package com.example.auth.domain.usecase
 
+import app.cash.turbine.test
 import com.core.util.Resource
 import com.example.auth.data.model.ResOtp
 import com.example.auth.domain.testutils.Constants
 import com.example.auth.fakes.PhoneAuthRepoFakeImpl
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
@@ -21,59 +23,56 @@ class SendOtpUseCaseTest {
 
     @Test
     fun `On failure emit error` () = runTest {
-        val events = mutableListOf<Resource<Boolean>>()
-        sendOtpUseCase.invoke(Constants.SEND_OTP_FAIL_PHONE_NO).collect {
-            events.add(it)
-        }
-        val firstEmission = events.getOrNull(0)
-        val secondEmission =  events.getOrNull(1)
-        if(firstEmission is Resource.Loading && secondEmission is Resource.Error) {
-            assert(true)
-        } else {
-            assert(false)
+        sendOtpUseCase.invoke(Constants.SEND_OTP_FAIL_PHONE_NO).test {
+            val firstEmission = awaitItem()
+            val secondEmission = awaitItem()
+            awaitComplete()
+            if(firstEmission is Resource.Loading && secondEmission is Resource.Error) {
+                assert(true)
+            } else {
+                assert(false)
+            }
         }
     }
 
     @Test
     fun `On null response emit error` () = runTest {
-        val events = mutableListOf<Resource<Boolean>>()
-        sendOtpUseCase.invoke(Constants.SEND_OTP_FAIL_PHONE_NO).collect {
-            events.add(it)
-        }
-        val firstEmission = events.getOrNull(0)
-        val secondEmission =  events.getOrNull(1)
-        if(firstEmission is Resource.Loading && secondEmission is Resource.Error) {
-            assert(true)
-        } else {
-            assert(false)
+        sendOtpUseCase.invoke(Constants.SEND_OTP_NULL_PHONE_NO).test {
+            val firstEmission = awaitItem()
+            val secondEmission = awaitItem()
+            awaitComplete()
+            if(firstEmission is Resource.Loading && secondEmission is Resource.Error) {
+                assert(true)
+            } else {
+                assert(false)
+            }
         }
     }
 
     @Test
     fun `On success emit success` () = runTest {
-        val events = mutableListOf<Resource<Boolean>>()
-        sendOtpUseCase.invoke(Constants.SEND_OTP_SUCCESS_PHONE_NO).collect {
-            events.add(it)
-        }
-        val firstEmission = events.getOrNull(0)
-        val secondEmission =  events.getOrNull(1)
-        if(firstEmission is Resource.Loading && secondEmission is Resource.Success) {
-            assert(true)
-        } else {
-            assert(false)
+        sendOtpUseCase.invoke(Constants.SEND_OTP_SUCCESS_PHONE_NO).test {
+            val firstEmission = awaitItem()
+            val secondEmission = awaitItem()
+            awaitComplete()
+            if(firstEmission is Resource.Loading && secondEmission is Resource.Success) {
+                assert(true)
+            } else {
+                assert(false)
+            }
         }
     }
 
     @Test
     fun `On Invalid number emit error` () = runTest {
-        val events = mutableListOf<Resource<Boolean>>()
-        sendOtpUseCase.invoke(Constants.SEND_OTP_WRONG_PHONE_NO).collect {
-            events.add(it)
-        }
-        if(events.size == 1 && events[0] is Resource.Error) {
-            assert(true)
-        } else {
-            assert(false)
+        sendOtpUseCase.invoke(Constants.SEND_OTP_WRONG_PHONE_NO).test {
+            val firstEmission = awaitItem()
+            awaitComplete()
+            if(firstEmission is Resource.Error) {
+                assert(true)
+            } else{
+                assert(false)
+            }
         }
     }
 
