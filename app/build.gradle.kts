dbdfd7447add0592a24b0943ff55b9eb897eba13
Sudhanshu Kumar <rishuparashar7@gmail.com>
2025-08-28 04:32:20 +0530

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ev.greenh"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "2.4"
+        versionCode = 18
+        versionName = "2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
